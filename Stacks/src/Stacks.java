ba9dfb695ffd6a3b4179eb7930386b7dfdf460c5
TomatoTwister <hemantjatal59@gmail.com>
2023-12-21 13:55:23 +0530

    first commit

---
+++ Stacks/src/Stacks.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(23);
+        st.push(33);
+        st.push(9);
+        st.push(11);
+        System.out.println(st);
+
+    }
+    
+}
